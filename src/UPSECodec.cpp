662395341b8f3e0b44902f7e0e1955f635785873
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-01-06 12:00:32 +0100

    update include paths

--- src/UPSECodec.cpp
+++ src/UPSECodec.cpp
@@ -17,7 +17,7 @@
  *
  */
 
-#include "kodi/libXBMC_addon.h"
+#include "libXBMC_addon.h"
 #include <iostream>
 
 extern "C" {
@@ -26,7 +26,7 @@ extern "C" {
 #include <stdio.h>
 #include <stdint.h>
 
-#include "kodi/kodi_audiodec_dll.h"
+#include "kodi_audiodec_dll.h"
 
 ADDON::CHelper_libXBMC_addon *XBMC           = NULL;
 
