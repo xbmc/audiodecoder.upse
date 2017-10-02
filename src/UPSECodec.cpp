0cf4e2d3742a27d4fcd80eb2ff9553a2eb6b2546
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2017-10-02 23:21:27 +0200

    update to c++ bindings

--- src/UPSECodec.cpp
+++ src/UPSECodec.cpp
@@ -17,7 +17,8 @@
  *
  */
 
-#include "libXBMC_addon.h"
+#include <kodi/addon-instance/AudioDecoder.h>
+#include <kodi/Filesystem.h>
 #include <iostream>
 
 extern "C" {
@@ -26,218 +27,162 @@ extern "C" {
 #include <stdio.h>
 #include <stdint.h>
 
-#include "kodi_audiodec_dll.h"
 
-ADDON::CHelper_libXBMC_addon *XBMC           = NULL;
-
-ADDON_STATUS ADDON_Create(void* hdl, void* props)
+void* kodi_vfs_open(const char* path, const char* mode)
 {
-  if (!XBMC)
-    XBMC = new ADDON::CHelper_libXBMC_addon;
-
-  if (!XBMC->RegisterMe(hdl))
+  kodi::vfs::CFile* file = new kodi::vfs::CFile;
+  if (!file->OpenFile(path, 0))
   {
-    delete XBMC, XBMC=NULL;
-    return ADDON_STATUS_PERMANENT_FAILURE;
+    delete file;
+    return nullptr;
   }
-
-  return ADDON_STATUS_OK;
+  return file;
 }
 
-//-- Stop ---------------------------------------------------------------------
-// This dll must cease all runtime activities
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-void ADDON_Stop()
+size_t kodi_vfs_read(void* ptr, size_t size, size_t nmemb, void* file)
 {
+  kodi::vfs::CFile* cfile = static_cast<kodi::vfs::CFile*>(file);
+  return cfile->Read(ptr, size*nmemb)/size;
 }
 
-//-- Destroy ------------------------------------------------------------------
-// Do everything before unload of this add-on
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-void ADDON_Destroy()
+int kodi_vfs_seek(void* file, long offset, int whence)
 {
-  XBMC=NULL;
+  kodi::vfs::CFile* cfile = static_cast<kodi::vfs::CFile*>(file);
+  return cfile->Seek(offset, whence);
 }
 
-//-- HasSettings --------------------------------------------------------------
-// Returns true if this add-on use settings
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-bool ADDON_HasSettings()
+int kodi_vfs_close(void* file)
 {
-  return false;
-}
-
-//-- GetStatus ---------------------------------------------------------------
-// Returns the current Status of this visualisation
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-ADDON_STATUS ADDON_GetStatus()
-{
-  return ADDON_STATUS_OK;
-}
-
-//-- GetSettings --------------------------------------------------------------
-// Return the settings for XBMC to display
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-unsigned int ADDON_GetSettings(ADDON_StructSetting ***sSet)
-{
+  delete static_cast<kodi::vfs::CFile*>(file);
   return 0;
 }
 
-//-- FreeSettings --------------------------------------------------------------
-// Free the settings struct passed from XBMC
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-
-void ADDON_FreeSettings()
+long kodi_vfs_tell(void* file)
 {
-}
-
-//-- SetSetting ---------------------------------------------------------------
-// Set a specific Setting value (called from XBMC)
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-ADDON_STATUS ADDON_SetSetting(const char *strSetting, const void* value)
-{
-  return ADDON_STATUS_OK;
-}
-
-//-- Announce -----------------------------------------------------------------
-// Receive announcements from XBMC
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-void ADDON_Announce(const char *flag, const char *sender, const char *message, const void *data)
-{
-}
-
-void* xbmc_vfs_open(const char* path, const char* mode)
-{
-  return XBMC->OpenFile(path, 0);
-}
-
-size_t xbmc_vfs_read(void* ptr, size_t size, size_t nmemb, void* file)
-{
-  return XBMC->ReadFile(file, ptr, size*nmemb)/size;
-}
-
-int xbmc_vfs_seek(void* file, long offset, int whence)
-{
-  return XBMC->SeekFile(file, offset, whence);
-}
-
-int xbmc_vfs_close(void* file)
-{
-  XBMC->CloseFile(file);
-  return 0;
-}
-
-long xbmc_vfs_tell(void* file)
-{
-  return XBMC->GetFilePosition(file);
+  kodi::vfs::CFile* cfile = static_cast<kodi::vfs::CFile*>(file);
+  return cfile->GetPosition();
 }
 
 const upse_iofuncs_t upse_io =
 {
-  xbmc_vfs_open,
-  xbmc_vfs_read,
-  xbmc_vfs_seek,
-  xbmc_vfs_close,
-  xbmc_vfs_tell
+  kodi_vfs_open,
+  kodi_vfs_read,
+  kodi_vfs_seek,
+  kodi_vfs_close,
+  kodi_vfs_tell
 };
 
 struct UPSEContext
 {
-  upse_module_t* mod;
+  upse_module_t* mod = nullptr;
   int16_t* buf;
   int16_t* head;
   int size;
 };
-             
-void* Init(const char* strFile, unsigned int filecache, int* channels,
-           int* samplerate, int* bitspersample, int64_t* totaltime,
-           int* bitrate, AEDataFormat* format, const AEChannel** channelinfo)
-{
-  upse_module_init();
-  upse_module_t* upse = upse_module_open(strFile, &upse_io);
-  if (!upse)
-    return NULL;
 
-  UPSEContext* result = new UPSEContext;
-  result->mod = upse;
-  result->size = 0;
-  result->head = result->buf;
-
-  upse_spu_state_t * p_spu = reinterpret_cast<upse_spu_state_t *> ( upse->instance.spu );
-  upse_ps1_spu_setvolume( p_spu, 32 );
-
-  *totaltime = upse->metadata->length;
-  static enum AEChannel map[3] = {
-    AE_CH_FL, AE_CH_FR, AE_CH_NULL
-  };
-  *format = AE_FMT_S16NE;
-  *channelinfo = map;
-  *channels = 2;
-  *bitspersample = 16;
-  *bitrate = 0.0;
-  *samplerate = 44100;
-
-  return result;
 }
 
-int ReadPCM(void* context, uint8_t* pBuffer, int size, int *actualsize)
+class CUPSECodec : public kodi::addon::CInstanceAudioDecoder,
+                   public kodi::addon::CAddonBase
 {
-  UPSEContext* upse = (UPSEContext*)context;
+public:
+  CUPSECodec(KODI_HANDLE instance) :
+    CInstanceAudioDecoder(instance) {}
 
-  if (upse->size == 0)
+  virtual ~CUPSECodec()
   {
-    upse->size = 4*upse_eventloop_render(upse->mod, (int16_t**)&upse->buf);
-    upse->head = upse->buf;
+    if (ctx.mod)
+    {
+      upse_eventloop_stop(ctx.mod);
+      upse_eventloop_render(ctx.mod, (int16_t**)&ctx.buf);
+      upse_module_close(ctx.mod);
+    }
   }
-#undef min
-  *actualsize = std::min(upse->size, size);
-  memcpy(pBuffer, upse->head, *actualsize);
-  upse->head += *actualsize/2;
-  upse->size -= *actualsize;
-  return 0;
-}
 
-int64_t Seek(void* context, int64_t time)
-{
-  UPSEContext* upse = (UPSEContext*)context;
-  upse_eventloop_seek(upse->mod, time);
-  return time;
-}
-
-bool DeInit(void* context)
-{
-  UPSEContext* upse = (UPSEContext*)context;
-  upse_eventloop_stop(upse->mod);
-  upse_eventloop_render(upse->mod, (int16_t**)&upse->buf);
-  upse_module_close(upse->mod);
-  delete upse;
-
-  return true;
-}
-
-bool ReadTag(const char* strFile, char* title, char* artist, int* length)
-{
-  upse_psf_t* tag = upse_get_psf_metadata(strFile, &upse_io);
-  if (tag)
+  virtual bool Init(const std::string& filename, unsigned int filecache,
+                    int& channels, int& samplerate,
+                    int& bitspersample, int64_t& totaltime,
+                    int& bitrate, AEDataFormat& format,
+                    std::vector<AEChannel>& channellist) override
   {
-    strcpy(title, tag->title);
-    strcpy(artist, tag->artist);
-    *length = tag->length/1000;
+    upse_module_init();
+    upse_module_t* upse = upse_module_open(filename.c_str(), &upse_io);
+    if (!upse)
+      return false;
+
+    ctx.mod = upse;
+    ctx.size = 0;
+    ctx.head = ctx.buf;
+
+    upse_spu_state_t * p_spu = reinterpret_cast<upse_spu_state_t *> ( upse->instance.spu );
+    upse_ps1_spu_setvolume( p_spu, 32 );
+
+    totaltime = upse->metadata->length;
+    format = AE_FMT_S16NE;
+    channellist = { AE_CH_FL, AE_CH_FR };
+    channels = 2;
+    bitspersample = 16;
+    bitrate = 0.0;
+    samplerate = 44100;
+
     return true;
   }
-  return false;
-}
 
-int TrackCount(const char* strFile)
+  virtual int ReadPCM(uint8_t* buffer, int size, int& actualsize) override
+  {
+    if (ctx.size == 0)
+    {
+      ctx.size = 4*upse_eventloop_render(ctx.mod, (int16_t**)&ctx.buf);
+      ctx.head = ctx.buf;
+    }
+#undef min
+    actualsize = std::min(ctx.size, size);
+    memcpy(buffer, ctx.head, actualsize);
+    ctx.head += actualsize/2;
+    ctx.size -= actualsize;
+    return 0;
+  }
+
+  virtual int64_t Seek(int64_t time) override
+  {
+    upse_eventloop_seek(ctx.mod, time);
+    return time;
+  }
+
+  virtual bool ReadTag(const std::string& file, std::string& title,
+                       std::string& artist, int& length) override
+  {
+    upse_psf_t* tag = upse_get_psf_metadata(file.c_str(), &upse_io);
+    if (tag)
+    {
+      title = tag->title;
+      artist = tag->artist;
+      length = tag->length/1000;
+      delete tag;
+      return true;
+    }
+
+    return false;
+  }
+
+private:
+  UPSEContext ctx;
+};
+
+
+class ATTRIBUTE_HIDDEN CMyAddon : public kodi::addon::CAddonBase
 {
-  return 1;
-}
-}
+public:
+  CMyAddon() { }
+  virtual ADDON_STATUS CreateInstance(int instanceType, std::string instanceID, KODI_HANDLE instance, KODI_HANDLE& addonInstance) override
+  {
+    addonInstance = new CUPSECodec(instance);
+    return ADDON_STATUS_OK;
+  }
+  virtual ~CMyAddon()
+  {
+  }
+};
+
+
+ADDONCREATOR(CMyAddon);
