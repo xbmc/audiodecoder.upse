90a9680ce9f0190a6223b1d2f5f9e269acd32b5e
Alwin Esch <alwin.esch@web.de>
2020-07-29 12:50:33 +0200

    clang format and code cleanup

--- src/UPSECodec.cpp
+++ src/UPSECodec.cpp
@@ -6,175 +6,161 @@
  *  See LICENSE.md for more information.
  */
 
-#include <kodi/addon-instance/AudioDecoder.h>
+#include "UPSECodec.h"
+
 #include <kodi/Filesystem.h>
-#include <algorithm>
-#include <iostream>
 
-extern "C" {
-#include "upse.h"
-#include <upse-internal.h>
-#include <stdio.h>
-#include <stdint.h>
-
-
-void* kodi_vfs_open(const char* path, const char* mode)
+extern "C"
 {
-  kodi::vfs::CFile* file = new kodi::vfs::CFile;
-  if (!file->OpenFile(path, 0))
+
+  void* kodi_vfs_open(const char* path, const char* mode)
   {
-    delete file;
-    return nullptr;
-  }
-  return file;
-}
-
-size_t kodi_vfs_read(void* ptr, size_t size, size_t nmemb, void* file)
-{
-  kodi::vfs::CFile* cfile = static_cast<kodi::vfs::CFile*>(file);
-  return cfile->Read(ptr, size*nmemb)/size;
-}
-
-int kodi_vfs_seek(void* file, long offset, int whence)
-{
-  kodi::vfs::CFile* cfile = static_cast<kodi::vfs::CFile*>(file);
-  return cfile->Seek(offset, whence);
-}
-
-int kodi_vfs_close(void* file)
-{
-  delete static_cast<kodi::vfs::CFile*>(file);
-  return 0;
-}
-
-long kodi_vfs_tell(void* file)
-{
-  kodi::vfs::CFile* cfile = static_cast<kodi::vfs::CFile*>(file);
-  return cfile->GetPosition();
-}
-
-const upse_iofuncs_t upse_io =
-{
-  kodi_vfs_open,
-  kodi_vfs_read,
-  kodi_vfs_seek,
-  kodi_vfs_close,
-  kodi_vfs_tell
-};
-
-struct UPSEContext
-{
-  upse_module_t* mod = nullptr;
-  int16_t* buf = nullptr;
-  int16_t* head = nullptr;
-  int size = 0;
-};
-
-}
-
-class ATTRIBUTE_HIDDEN CUPSECodec : public kodi::addon::CInstanceAudioDecoder
-{
-public:
-  CUPSECodec(KODI_HANDLE instance, const std::string& version) :
-    CInstanceAudioDecoder(instance, version) {}
-
-  virtual ~CUPSECodec()
-  {
-    if (ctx.mod)
+    kodi::vfs::CFile* file = new kodi::vfs::CFile;
+    if (!file->OpenFile(path, 0))
     {
-      upse_eventloop_stop(ctx.mod);
-      if (!m_endWasReached)
-        upse_eventloop_render(ctx.mod, (int16_t**)&ctx.buf);
-      upse_module_close(ctx.mod);
+      delete file;
+      return nullptr;
     }
+    return file;
   }
 
-  bool Init(const std::string& filename, unsigned int filecache,
-            int& channels, int& samplerate,
-            int& bitspersample, int64_t& totaltime,
-            int& bitrate, AudioEngineDataFormat& format,
-            std::vector<AudioEngineChannel>& channellist) override
+  size_t kodi_vfs_read(void* ptr, size_t size, size_t nmemb, void* file)
   {
-    upse_module_init();
-    upse_module_t* upse = upse_module_open(filename.c_str(), &upse_io);
-    if (!upse)
-    {
-      m_endWasReached = true;
-      return false;
-    }
-
-    ctx.mod = upse;
-    ctx.size = 0;
-    ctx.head = ctx.buf;
-
-    upse_spu_state_t * p_spu = reinterpret_cast<upse_spu_state_t *> ( upse->instance.spu );
-    upse_ps1_spu_setvolume( p_spu, 32 );
-
-    totaltime = upse->metadata->length;
-    format = AUDIOENGINE_FMT_S16NE;
-    channellist = { AUDIOENGINE_CH_FL, AUDIOENGINE_CH_FR };
-    channels = 2;
-    bitspersample = 16;
-    bitrate = 0.0;
-    samplerate = 44100;
-
-    return true;
+    kodi::vfs::CFile* cfile = static_cast<kodi::vfs::CFile*>(file);
+    return cfile->Read(ptr, size * nmemb) / size;
   }
 
-  int ReadPCM(uint8_t* buffer, int size, int& actualsize) override
+  int kodi_vfs_seek(void* file, long offset, int whence)
   {
-    if (ctx.size == 0)
-    {
-      ctx.size = 4*upse_eventloop_render(ctx.mod, (int16_t**)&ctx.buf);
-      ctx.head = ctx.buf;
+    kodi::vfs::CFile* cfile = static_cast<kodi::vfs::CFile*>(file);
+    return cfile->Seek(offset, whence);
+  }
 
-      // If return against 0, the end of stream is reached
-      if (ctx.size == 0)
-      {
-        m_endWasReached = true;
-        return 1;
-      }
-    }
-#undef min
-    actualsize = std::min(ctx.size, size);
-    memcpy(buffer, ctx.head, actualsize);
-    ctx.head += actualsize/2;
-    ctx.size -= actualsize;
+  int kodi_vfs_close(void* file)
+  {
+    delete static_cast<kodi::vfs::CFile*>(file);
     return 0;
   }
 
-  int64_t Seek(int64_t time) override
+  long kodi_vfs_tell(void* file)
   {
-    upse_eventloop_seek(ctx.mod, time);
-    return time;
+    kodi::vfs::CFile* cfile = static_cast<kodi::vfs::CFile*>(file);
+    return cfile->GetPosition();
   }
 
-  bool ReadTag(const std::string& filename, kodi::addon::AudioDecoderInfoTag& tag) override
-  {
-    upse_psf_t* upseTag = upse_get_psf_metadata(filename.c_str(), &upse_io);
-    if (upseTag)
-    {
-      tag.SetTitle(upseTag->title);
-      tag.SetArtist(upseTag->artist);
-      tag.SetDuration(upseTag->length/1000);
-      delete upseTag;
-      return true;
-    }
+  const upse_iofuncs_t upse_io = {kodi_vfs_open, kodi_vfs_read, kodi_vfs_seek, kodi_vfs_close,
+                                  kodi_vfs_tell};
 
+} /* extern "C" */
+
+//------------------------------------------------------------------------------
+
+CUPSECodec::CUPSECodec(KODI_HANDLE instance, const std::string& version)
+  : CInstanceAudioDecoder(instance, version)
+{
+}
+
+CUPSECodec::~CUPSECodec()
+{
+  if (ctx.mod)
+  {
+    upse_eventloop_stop(ctx.mod);
+    if (!m_endWasReached)
+      upse_eventloop_render(ctx.mod, (int16_t**)&ctx.buf);
+    upse_module_close(ctx.mod);
+  }
+}
+
+bool CUPSECodec::Init(const std::string& filename,
+                      unsigned int filecache,
+                      int& channels,
+                      int& samplerate,
+                      int& bitspersample,
+                      int64_t& totaltime,
+                      int& bitrate,
+                      AudioEngineDataFormat& format,
+                      std::vector<AudioEngineChannel>& channellist)
+{
+  upse_module_init();
+  upse_module_t* upse = upse_module_open(filename.c_str(), &upse_io);
+  if (!upse)
+  {
+    m_endWasReached = true;
     return false;
   }
 
-private:
-  UPSEContext ctx;
-  bool m_endWasReached = false;
-};
+  ctx.mod = upse;
+  ctx.size = 0;
+  ctx.head = ctx.buf;
 
+  upse_spu_state_t* p_spu = reinterpret_cast<upse_spu_state_t*>(upse->instance.spu);
+  upse_ps1_spu_setvolume(p_spu, 32);
+
+  totaltime = upse->metadata->length;
+  format = AUDIOENGINE_FMT_S16NE;
+  channellist = {AUDIOENGINE_CH_FL, AUDIOENGINE_CH_FR};
+  channels = 2;
+  bitspersample = 16;
+  bitrate = 0.0;
+  samplerate = 44100;
+
+  return true;
+}
+
+int CUPSECodec::ReadPCM(uint8_t* buffer, int size, int& actualsize)
+{
+  if (ctx.size == 0)
+  {
+    ctx.size = 4 * upse_eventloop_render(ctx.mod, (int16_t**)&ctx.buf);
+    ctx.head = ctx.buf;
+
+    // If return against 0, the end of stream is reached
+    if (ctx.size == 0)
+    {
+      m_endWasReached = true;
+      return 1;
+    }
+  }
+#undef min
+  actualsize = std::min(ctx.size, size);
+  memcpy(buffer, ctx.head, actualsize);
+  ctx.head += actualsize / 2;
+  ctx.size -= actualsize;
+  return 0;
+}
+
+int64_t CUPSECodec::Seek(int64_t time)
+{
+  upse_eventloop_seek(ctx.mod, time);
+  return time;
+}
+
+bool CUPSECodec::ReadTag(const std::string& filename, kodi::addon::AudioDecoderInfoTag& tag)
+{
+  upse_psf_t* upseTag = upse_get_psf_metadata(filename.c_str(), &upse_io);
+  if (upseTag)
+  {
+    tag.SetTitle(upseTag->title);
+    tag.SetArtist(upseTag->artist);
+    tag.SetDuration(upseTag->length / 1000);
+    delete upseTag;
+    return true;
+  }
+
+  return false;
+}
+
+//------------------------------------------------------------------------------
 
 class ATTRIBUTE_HIDDEN CMyAddon : public kodi::addon::CAddonBase
 {
 public:
   CMyAddon() = default;
-  virtual ADDON_STATUS CreateInstance(int instanceType, const std::string& instanceID, KODI_HANDLE instance, const std::string& version, KODI_HANDLE& addonInstance) override
+  virtual ADDON_STATUS CreateInstance(int instanceType,
+                                      const std::string& instanceID,
+                                      KODI_HANDLE instance,
+                                      const std::string& version,
+                                      KODI_HANDLE& addonInstance) override
   {
     addonInstance = new CUPSECodec(instance, version);
     return ADDON_STATUS_OK;
@@ -182,5 +168,4 @@ public:
   virtual ~CMyAddon() = default;
 };
 
-
-ADDONCREATOR(CMyAddon);
+ADDONCREATOR(CMyAddon)
